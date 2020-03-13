b1cbf60d6608bf84751cab75143b968f58abee8c
Mike Bonitati <mbon419@gmail.com>
2020-03-12 20:02:12 -0400

    Built out RecipeController to recieve user input to map to the API call to get the recipe that corresponds with it

--- KitchenSink/Controllers/RecipesController.cs
+++ KitchenSink/Controllers/RecipesController.cs
@@ -13,24 +13,39 @@ namespace KitchenSink.Controllers
 {
     public class RecipesController : Controller
     {
+        //Recipes recipes = new Recipes();
+        //List<Recipes> recipeList = new List<Recipes>();
+        //private JsonDocument jDoc;
         private IConfiguration _config;
 
+        // 3/8/2020 edits using JsonPropertyName
         RecipeArray recipe = new RecipeArray();
 
         public RecipesController(IConfiguration config)
         {
             _config = config;
         }
-        public async Task<IActionResult> GetRandomRecipe()
+        public async Task<IActionResult> GetRandomRecipe(string protein, string starch, string veggie, string spice, string aromatic)
         {
-            var key = _config["SpoonacularApiKey"];
+            var SpoonApiKey = _config["SpoonacularApiKey"];
+            //this using block sets the stage to open the API call
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync($"https://api.spoonacular.com/recipes/random?number=1&tags=vegetarian,dessert&apiKey={key}"))
-                {
-                    var stringResponse = await response.Content.ReadAsStringAsync();
-                    recipe = JsonSerializer.Deserialize<RecipeArray>(stringResponse);
-                }
+                //this using block is the action to get the call.  We have added our key to the end of the request URL per API documentation requirement.
+                //this specific url generates 1 random recipe everytime we call it.
+                using var response = await httpClient.GetAsync($"https://api.spoonacular.com/recipes/findByIngredients?ingredients={protein},+{starch},+{veggie},+{spice},+{aromatic}&number=1&apiKey=[SpoonApiKey]");
+                var stringResponse = await response.Content.ReadAsStringAsync();
+
+                //cookbook = new Recipes
+
+                //Parses the text representing a single JSON value into an
+                //an instance of the type specified by a generic type parameter
+
+                //cookbook = parsed string response
+                //return as Recipes object
+                //which consists of strings and ints
+
+                recipe = JsonSerializer.Deserialize<RecipeArray>(stringResponse);
             }
             return View(recipe);
         }
